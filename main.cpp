ddc8734b14848f52fae848af19bc1db1cf23a542
UKyz <ukyz@icloud.com>
2018-04-03 18:09:07 +0200

    Suppression du game dans le main.cpp

--- main.cpp
+++ main.cpp
@@ -9,24 +9,15 @@
 #include <QGraphicsPixmapItem>
 #include <QTimer>
 #include <QVBoxLayout>
-
 #include <QApplication>
-
-//Game * game;
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     View w;
     Model m;
-
-    // On crée un objet game (QGraphicsView contenant un QGraphicsScene contenant nos images de Schtroumpf)
-    //game = new Game();
-
     Controller(&m, &w);
 
-    // On met l'objet en tant qu'objet principal de la view
-    //w.setCentralWidget(game);
     w.show();
 
     return a.exec();
